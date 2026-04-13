a52c108fe442ce0dae91c9d3f19bdbe3326e6dc0
Uzrt <worldsilvertree@gmail.com>
2026-04-13 18:42:28 +0900

    API 응답 기초 설정

--- lemon/mission/src/main/java/com/springboot/mission/global/apiPayload/ApiResponse.java
+++ lemon/mission/src/main/java/com/springboot/mission/global/apiPayload/ApiResponse.java
@@ -1,4 +1,33 @@
 package com.springboot.mission.global.apiPayload;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.springboot.mission.global.apiPayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+
+    @JsonProperty("code")
+    private final String code;
+
+    @JsonProperty("message")
+    private final String message;
+
+    @JsonProperty("result")
+    private final T result;
+
+    public static <T> ApiResponse<T> isSuccess(BaseErrorCode code, T result) {
+        return new ApiResponse<>(true, code.getCode(), code.getMessage(), result);
+    }
+
+    public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result) {
+        return new ApiResponse<>(false, code.getCode(), code.getMessage(), result);
+    }
 }
