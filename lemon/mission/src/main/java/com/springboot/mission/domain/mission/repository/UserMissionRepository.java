cb5a28191fadbcc16ae5c9dd8adf4aefa77696b1
Uzrt <worldsilvertree@gmail.com>
2026-05-25 00:21:13 +0900

    UserMissionRepository 수정

--- lemon/mission/src/main/java/com/springboot/mission/domain/mission/repository/UserMissionRepository.java
+++ lemon/mission/src/main/java/com/springboot/mission/domain/mission/repository/UserMissionRepository.java
@@ -3,16 +3,14 @@ package com.springboot.mission.domain.mission.repository;
 import com.springboot.mission.domain.mission.entity.UserMission;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 public interface UserMissionRepository extends JpaRepository<UserMission, Long> {
+    Page<UserMission> findAllByUserIdAndIsCompletedFalse(Long userId, Pageable pageable);
 
-    Page<UserMission> findAllByUserIdAndIsCompletedFalse(Long userId, PageRequest pageRequest);
-
-    Page<UserMission> findAllByUserId(Long userId, PageRequest pageRequest);
+    Page<UserMission> findAllByUserId(Long userId, Pageable pageable);
 
     Optional<UserMission> findByUserIdAndMissionId(Long userId, Long missionId);
 }
